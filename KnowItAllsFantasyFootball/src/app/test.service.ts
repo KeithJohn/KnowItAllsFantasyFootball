ec074c82fd559a04c68b41c4913e1d4ee6228f66
KeithJohn <keithjohn1020@gmail.com>
2019-11-14 16:29:11 -0600

    added comments for getting certain stats

--- KnowItAllsFantasyFootball/src/app/test.service.ts
+++ KnowItAllsFantasyFootball/src/app/test.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PlayersPageComponent } from './view/players-page/players-page.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,19 @@ export class TestService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * stats[2].appliedTotal is projected score for week
+   * stats[3].appliedTotal is actual scored for week
+   */
   getFreeAgent(){
-    this.http.get('https://fantasy.espn.com/apis/v3/games/ffl/seasons/2019/segments/0/leagues/58438855?scoringPeriodId=2&view=kona_player_info').subscribe(data => {
-      console.log(data);
+    this.http.get('https://fantasy.espn.com/apis/v3/games/ffl/seasons/2019/segments/0/leagues/58438855?scoringPeriodId=10&view=kona_player_info').subscribe(data => {
+      let players = data['players'];
+      for (let i = 0; i < 50; i++){
+        console.log(players[i].player.fullName);
+        console.log(players[i].player.stats);
+      }
+
+      //console.log(data);
     });
   }
 }
